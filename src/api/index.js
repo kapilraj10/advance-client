8029daa209e77f7748be4c5887acc12c356e16f1
kapil <kapilrajkc10@gmail.com>
2025-12-29 02:31:10 +0545

    Update index.js

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const API = axios.create({
-    baseURL: import.meta.env.VITE_API_URL || ''
+    baseURL: import.meta.env.VITE_API_URL || 'https://advance-backend-coral.vercel.app/'
 })
 
 export function setAuthToken(token) {
